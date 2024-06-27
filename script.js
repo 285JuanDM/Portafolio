f169d2237c488086f56cdf32469a514f71f62aa0
285JuanDM <285juanda@gmail.com>
2024-06-26 23:14:49 -0500

    actualizar la interactividad de la barra de navegación y el desplazamiento suave

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const menuLinks = document.querySelectorAll('.nav-menu a[href^="#"]');
+
 const observador = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
         const id = entry.target.getAttribute('id');
         const menuLink = document.querySelector(`.nav-menu a[href="#${id}"]`);
+
         if (entry.isIntersecting) {
             document.querySelector(".nav-menu a.active").classList.remove("active");
             menuLink.classList.add("active");
         }
+
     });
-}, {rootMargin: "-60% 0% -40% 0%"});
+}, {rootMargin: "-50% 0% -50% 0%"});
 
 menuLinks.forEach(link => {
     const hash = link.getAttribute('href');
+
     const target = document.querySelector(hash);
     if (target) {
         observador.observe(target);
